1c97dea977634ad26f57feecc7497ad73445258b
Code-gurl <102672669+Natacha-Danbadan@users.noreply.github.com>
2023-01-17 09:47:04 +0100

    Update SkillsItem.jsx
    Fixes

--- src/components/SkillsItem.jsx
+++ src/components/SkillsItem.jsx
@@ -2,7 +2,7 @@
 const SkillsItem =({ skillsobj }) => {
     let {source, name} = skillsobj
   return (
-    <div className="flex min-w-[145px] xs:min-w-[44%] sm:min-w-[150px] md:min-w-[160px] lg:min-w-[200px] items-center justify-center bg-primaryTwo rounded-[6px] gap-2 lg:gap-4 py-6 md:py-8 lg:py-12 px-2 lg:px-4 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+    <div className="flex min-w-[140px] xs:min-w-[44%] sm:min-w-[150px] md:min-w-[160px] lg:min-w-[200px] items-center justify-center bg-primaryTwo rounded-[6px] gap-2 lg:gap-4 py-6 md:py-8 lg:py-12 px-2 lg:px-4 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
 
       <div className="w-8 lg:w-12 aspect-square">
         <img src={ source } alt={ name }  /> 
