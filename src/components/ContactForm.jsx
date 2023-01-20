6be424dfcf99777534358808cea9f30b006e061d
Natacha Danbadan <danbadannatacha@gmail.com>
2023-01-19 16:16:51 -0800

    fixes

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -11,17 +11,17 @@ function ContactForm() {
         <div className="flex flex-col lg:flex-row lg:w-4/5 gap-4 mx-auto items-stretch">
             <div className="w-full lg:w-1/2  flex flex-col justify-start gap-4 lg:justify-between">
                 <label htmlFor="Full Name">
-                    <input type="text" placeholder="Your full name" className="w-full placeholder-gray-700 focus:outline-none rounded-[8px] px-4 py-[16px]"/>
+                    <input type="text" placeholder="Your full name" className="w-full bg-white placeholder-gray-700 focus:outline-none rounded-[8px] px-4 py-[16px]"/>
                 </label>
                 <label htmlFor="Email Address">
-                    <input type="text" placeholder="Your email address" className="w-full placeholder-gray-700 focus:outline-none rounded-[8px] px-4 py-[16px]"/>
+                    <input type="text" placeholder="Your email address" className="w-full bg-white placeholder-gray-700 focus:outline-none rounded-[8px] px-4 py-[16px]"/>
                 </label>
                 <label htmlFor="Phone Number">
-                    <input  type="text" placeholder="Your phone number (optional)" className="w-full placeholder-gray-700 focus:outline-none rounded-[8px] px-4 py-[16px]"/>
+                    <input  type="text" placeholder="Your phone number (optional)" className="w-full bg-white placeholder-gray-700 focus:outline-none rounded-[8px] px-4 py-[16px]"/>
                 </label>
             </div>
             <div className="w-full lg:w-1/2">
-                <textarea className="w-full h-full rounded-[8px] px-4 py-[14px] placeholder-gray-700 focus:outline-none" name="message" id="" cols="30" rows="10" placeholder="Drop your Message Here">
+                <textarea className="w-full bg-white h-full rounded-[8px] px-4 py-[14px] placeholder-gray-700 focus:outline-none" name="message" id="" cols="30" rows="10" placeholder="Drop your Message Here">
 
                 </textarea>
             </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,15 @@
 
 import {FaHeart} from 'react-icons/fa'
-import logo from "../logo.png"
 
 function Footer() {
   const newYear = new Date().getFullYear();
   return (
     <div className="">
-            <nav className='w-full  md:px-14 px-8 bg-primaryTwo flex justify-between items-center'>
-              <div className="md:w-1/2 w-1/4 logo">
-                <img src={logo} alt="" className='md:w-20 w-4/5'/>
+            <footer className='w-full bg-primaryTwo'>
+              <div className="py-6 md:text-base items-center justify-center text-center text-xs flex font-semibold text-secondary">
+              portfolio &copy; {newYear} | Made with <FaHeart style={{color: 'red'}} /> Natacha
               </div>
-              <div className="md:w-1/2 w-3/4 hire-me-btn md:text-base text-xs flex justify-end font-semibold text-secondary">
-              portfolio &copy; {newYear} | Made with<FaHeart style={{color: 'red'}} /> Natacha
-              </div>
-            </nav>
+            </footer>
     </div>
   )
 }
